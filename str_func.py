d5dae23306d6eaf0e62a65abf8aab427e46c7c0b
Ilnar Khasanov <Khasanov1988@gmail.com>
2023-03-12 23:34:00 +0100

    Add to_title function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def to_upper(text: str):
+    """
+    Функция переводит все в верхний регистр
+    :param text:
+    :return:
+    """
     return text.upper()
+
+
+def to_title(text: str):
+    '''
+    Функция переводит первую букву в верхний регистр
+    :param text:
+    :return:
+    '''
+    return text.title()
